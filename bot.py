a6d3deec99fd0ace54d14d53a8bfbe75773c16f0
ayush-py-dev <clashofserver8823@gmail.com>
2023-05-05 10:15:33 +0530

    first bot commit

--- bot.py
+++ bot.py
@@ -8,6 +8,16 @@ import os
 
 load_dotenv()
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(
+    command_prefix='!',
+    intents=discord.Intents.all(),
+    case_insensitive=True,
+    status=discord.Status.idle,
+    activity=discord.Activity(type=discord.ActivityType.watching, name="my development")
+)
 
-bot.run()
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+bot.run(os.getenv('token'))
