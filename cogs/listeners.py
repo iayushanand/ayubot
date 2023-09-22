db7ec7c694373d5e0cb7546d01ea98de58582d40
iayushanand <iayushanand@users.noreply.github.com>
2023-09-22 20:07:37 +0000

    Format code with black

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -42,7 +42,7 @@ class Listeners(commands.Cog):
                     description=f"Welcome back {message.author.mention}! I removed your AFK status.",
                     color=discord.Color.blurple(),
                 ),
-                delete_after=5
+                delete_after=5,
             )
             try:
                 await message.author.edit(nick=message.author.display_name[5:])
