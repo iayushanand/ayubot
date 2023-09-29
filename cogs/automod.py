929f8d21df1c936c4e6b06f387790930c674583b
iayushanand <iayushanand@users.noreply.github.com>
2023-09-29 07:15:42 +0000

    Format code with black

--- cogs/automod.py
+++ cogs/automod.py
@@ -3,12 +3,13 @@ I made a new cog as I am aiming to make fairly complex auto mod system
 so I thought it's better to have them in one single file
 """
 
+import time
+
 import discord
 from discord.ext import commands
 
-import time
-
 from utils.helper import generate_id
+
 
 class Automod(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -20,16 +21,19 @@ class Automod(commands.Cog):
 
     def filter_words(self, filter_words: str, message: str) -> str:
         for word in filter_words:
-            word_length = len(word)//2
-            stars = "\*"*word_length
+            word_length = len(word) // 2
+            stars = "\*" * word_length
             message = message.replace(word, f"{stars}{word[word_length:]}")
         return message
 
-    async def send_new_message(self, channel: discord.TextChannel, message: discord.Message):
+    async def send_new_message(
+        self, channel: discord.TextChannel, message: discord.Message
+    ):
         webhook = await channel.create_webhook(name=message.author.display_name)
-        await webhook.send(content=message.content,
+        await webhook.send(
+            content=message.content,
             files=message.attachments,
-            avatar_url = message.author.display_avatar.url
+            avatar_url=message.author.display_avatar.url,
         )
         await webhook.delete()
 
@@ -53,18 +57,16 @@ class Automod(commands.Cog):
             if word.lower() in message.content.lower().split():
                 found_words.append(word)
         member = message.author
-        reason = "Used Banned word(s): `"+" ".join(found_words)+"`"
-        if len(found_words)!=0:
+        reason = "Used Banned word(s): `" + " ".join(found_words) + "`"
+        if len(found_words) != 0:
             await self.auto_warn(member=member, reason=reason)
         new_content = self.filter_words(banned_words, message.content.lower())
         if not message.content.lower() == new_content.lower():
             await message.delete()
             message.content = new_content
             channel = message.channel
-            await self.send_new_message(
-                channel = channel,
-                message = message
-            )
+            await self.send_new_message(channel=channel, message=message)
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Automod(bot=bot))
